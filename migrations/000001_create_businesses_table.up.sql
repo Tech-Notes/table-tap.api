0a92397a602d0360355b3173606a201ae022b933
Sai-Myo-Myat <saimyomyat511@gmail.com>
2025-04-03 08:50:57 +0700

    feat: create businesses table

---
+++ migrations/000001_create_businesses_table.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS businesses  (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    status VARCHAR(25) DEFAULT 'active',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
