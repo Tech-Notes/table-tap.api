534c520f0b6edd969d1f24c741e9535337d709ec
Sai-Myo-Myat <saimyomyat511@gmail.com>
2025-04-05 19:02:56 +0700

    feat: added hmac signature check

--- server/verify_middleware.go
+++ server/verify_middleware.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
 	"errors"
 	"net/http"
+	"os"
 )
 
 func verify(h http.Handler) http.Handler {
@@ -14,6 +18,28 @@ func verify(h http.Handler) http.Handler {
 			return
 		}
 
+		verifyWithHMACSignature(h).ServeHTTP(w, r)
+		return
+	})
+}
+
+func verifyWithHMACSignature(h http.Handler) http.Handler {
+	secretApiKey := os.Getenv("SECRET_API_KEY")
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		hmacSignature := generateHMACSignature(r.URL.Path, secretApiKey)
+
+		if hmacSignature != r.Header.Get("X-HMAC-SIGNATURE") {
+			writeError(w, http.StatusUnauthorized, errors.New("invalid HMAC signature"))
+			return
+		}
+
 		h.ServeHTTP(w, r)
 	})
+}
+
+func generateHMACSignature(data, secret string) string {
+	h := hmac.New(sha256.New, []byte(secret))
+	h.Write([]byte(data))
+	signature := hex.EncodeToString(h.Sum(nil))
+	return signature
 }
