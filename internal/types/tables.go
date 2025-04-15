c75ae620c3846eba5f1edaa105ab241bf67a7678
Sai-Myo-Myat <saimyomyat511@gmail.com>
2025-04-16 06:43:41 +0700

    feat: assign table status for consistency

--- internal/types/tables.go
+++ internal/types/tables.go
@@ -1,9 +1,19 @@
 package types
 
+type TableStatus string
+
+const (
+	TableStatusAvailable  TableStatus = "available"
+	TableStatusReserved   TableStatus = "reserved"
+	TableStatusOccupied   TableStatus = "occupied"     // When the table is currently in use
+	TableStatusCleaning   TableStatus = "cleaning"     // When the table is being cleaned
+	TableStatusOutOfOrder TableStatus = "out_of_order" // When the table is not usable
+)
+
 type Table struct {
-	ID         int64  `json:"id" db:"id"`
-	BusinessID int64  `json:"business_id" db:"business_id"`
-	Token      string `json:"token" db:"token"`
-	Status     string `json:"status" db:"status"`
-	QrCodeURL  string `json:"qr_code_url" db:"qr_code_url"`
+	ID         int64       `json:"id" db:"id"`
+	BusinessID int64       `json:"business_id" db:"business_id"`
+	Token      string      `json:"token" db:"token"`
+	Status     TableStatus `json:"status" db:"status"`
+	QrCodeURL  string      `json:"qr_code_url" db:"qr_code_url"`
 }
